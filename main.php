1162fec00e4bb86f2142b3e9034072334690d3ce
Naik-Waqar <waqarnaik3@gmail.com>
2019-11-26 18:09:42 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,33 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    echo "error connecting!";
+}else{
+    echo "Connection Success!<br><br>";
+}
+
+$sql = "SELECT id, name, email FROM customer";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
+?>
+
